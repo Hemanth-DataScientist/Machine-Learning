f2a10e6c155f95bb61ce1e709a41a6e2a6178e2b
Hemanth-DataScientist <swamygarih@gmail.com>
2024-11-15 12:51:48 +0530

    Update predictor.py

--- predictor.py
+++ predictor.py
@@ -1,10 +1,19 @@
+import warnings
+warnings.filterwarnings("ignore")
+
+import pandas as pd
+import numpy as np
+import requests
 import pickle
 
-with open('model_pickle2', 'rb') as f:
-    model = pickle.load(f)
+model_url = 'https://raw.githubusercontent.com/Hemanth-DataScientist/Machine-Learning/master/model_pickle2'
+response = requests.get(model_url, stream = True)
+response.raise_for_status()
+
+model = pickle.load(response.content)
 
 
-def predict_house_price(area, bedroom, age):
-    input_data = [[area, bedroom, age]]
+def predict_house_price(area,bedrooms,age):
+    input_data =[[area,bedrooms,age]]
     predicted_price = model.predict(input_data)
     return predicted_price[0]
